4c435b302e8716e8d099aa52dce6af8bb46d3ad1
ChaoHuang2018 <thomashuangchao@gmail.com>
2020-10-17 12:04:22 -0500

    minor

--- Activation.cpp
+++ Activation.cpp
@@ -16,11 +16,37 @@ Activation::Activation(string act, Interval in, Interval in_bound) {
 }
 
 Interval Activation::softplus(Interval x) {
+    double inf;
+    double sup;
 
+	mpfr_exp(inf, x.inf, MPFR_RNDD);
+    mpfr_add(inf, 1, inf, MPFR_RNDD);
+    inf = mpfr_log(inf, inf, MPFR_RNDD);
+
+	mpfr_exp(sup, x.sup, MPFR_RNDU);
+	sup = 1 + sup;
+    sup = mpfr_log(sup, sup, MPFR_RNDD);
+
+	Interval result(inf, sup);
+
+    return result;
 }
 
 Interval Activation::softplus_de(Interval x) {
+    double inf;
+    double sup;
 
+	mpfr_exp(inf, -x.inf, MPFR_RNDD);
+	inf = 1 + inf;
+    inf = mpfr_log(inf, inf, MPFR_RNDD);
+
+	mpfr_exp(sup, x.sup, MPFR_RNDU);
+	sup = 1 + sup;
+    sup = mpfr_log(sup, sup, MPFR_RNDD);
+
+	Interval result(inf, sup);
+
+    return result;
 }
 
 Interval Activation::softplus_de2(Interval x) {
