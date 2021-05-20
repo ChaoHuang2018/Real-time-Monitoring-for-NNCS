eaa4a5847c9c313e3c5329b50f3bf364a4f147bf
Jiameng <jmfan@bu.edu>
2021-05-19 21:40:18 -0400

    modify the run script

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 make && \
-    ./benchmark1_relu 0.05 50 4 6 # && \  Too large overestimation
+    ./benchmark1_relu 0.05 50 4 6 && \  # Too large overestimation
     ./benchmark1_relu_tanh 0.05 35 4 6 && \
 
-    ./benchmark1_sigmoid 0.05 35 4 6 # && \
+    ./benchmark1_sigmoid 0.05 35 4 6 && \
 
-    ./benchmark1_tanh 0.05 35 4 6 # && \  # Too large overestimation
+    ./benchmark1_tanh 0.05 35 4 6 && \  # Too large overestimation
     ./benchmark2_relu 0.1 10 4 6 && \
     ./benchmark2_relu_tanh 0.1 10 4 6 && \
     ./benchmark2_sigmoid 0.1 10 4 6 && \
