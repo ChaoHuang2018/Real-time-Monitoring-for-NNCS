840246794c8731c1aae5b2f2bf6b9c4309b1f396
Jiameng <jmfan@bu.edu>
2021-05-22 12:10:58 -0400

    symbolic exp

---
+++ run_exp.sh
@@ -0,0 +1,31 @@
+make && \
+    ./benchmark1_relu 0.1 50 4 6 0 && \
+    ./benchmark1_relu_tanh 0.1 35 4 6 0 && \
+    ./benchmark1_sigmoid 0.1 35 4 6 0 && \
+    ./benchmark1_tanh 0.1 25 4 6 0
+
+    ./benchmark2_relu 0.2 10 4 6 0 && \
+    ./benchmark2_relu_tanh 0.2 10 4 6 0 && \
+    ./benchmark2_sigmoid 0.2 10 4 6 0 && \
+    ./benchmark2_tanh 0.2 10 4 6 0 && \
+
+    ./benchmark3_relu 0.1 60 4 6 0 && \
+    ./benchmark3_relu_sigmoid 0.1 60 4 6 0 && \
+    ./benchmark3_sigmoid 0.1 60 4 6 0 && \
+    ./benchmark3_tanh 0.1 60 4 6 0 && \
+
+    ./benchmark4_relu 0.02 10 4 6 0 && \
+    ./benchmark4_relu_tanh 0.02 10 4 6 0 && \
+    ./benchmark4_sigmoid 0.02 10 4 6 0 && \
+    ./benchmark4_tanh 0.02 10 4 6 0 && \
+
+    ./benchmark5_relu 0.02 10 4 6 0 && \
+    ./benchmark5_relu_tanh 0.02 10 4 6 0 && \
+    ./benchmark5_sigmoid 0.02 10 4 6 0 && \
+    ./benchmark5_tanh 0.02 10 4 6 0 && \
+
+    ./benchmark6_relu 0.02 10 4 6 0 && \
+    ./benchmark6_relu_tanh 0.02 10 4 6 0 && \
+    ./benchmark6_sigmoid 0.02 10 4 6 0 && \
+    ./benchmark6_tanh 0.02 10 4 6 0
+#    ./nn_attitude_control_sigmoid
